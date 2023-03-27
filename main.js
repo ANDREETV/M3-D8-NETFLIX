33f0336f9d3aae8997304473bbf299ecc77038f2
AndreeTv <iorioandreaivrea@gmail.com>
2023-03-27 18:37:45 +0200

    secondo commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function animazione(params) {
+    let animazioneSezione1 = document.getElementById('animazione-sezione-1')
+
+    animazioneSezione1.classList.add('sezione-1')
+    animazioneSezione1.classList.add('sezione')
+
+    let animazioneSezione3 = document.getElementById('animazione-sezione-3')
+    animazioneSezione3.classList.add('sezione-1')
+    animazioneSezione3.classList.add('sezione')
+
+    let animazioneSezione2 = document.getElementById('animazione-sezione-2')
+    animazioneSezione2.classList.add('sezione-2')
+    animazioneSezione2.classList.add('sezione')
+}
+
+window.onload = function(){
+    animazione()
+}
